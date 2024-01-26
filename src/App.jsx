ea58984e9ef465ffa1d23a4376c8112e8b0b78be
vivek-panchal <vivek.nsut@gmail.com>
2024-01-26 14:03:16 +0530

    project setup using vite and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Price from './components/Price'
+
+function App() {
+  
+
+  return (
+    <>
+       
+      <Navbar />
+      <Hero />
+      <Content />
+      <Price/>
+      <Footer />
+    
+    </>
+  )
+}
+
+export default App
